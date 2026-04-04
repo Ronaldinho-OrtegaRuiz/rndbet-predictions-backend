a55e92d597628c918c2f685c30a5dc87c73410e0
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2026-04-04 12:24:01 -0500

    First commit

---
+++ src/main/java/rndbet/rndbetpredictionsbackend/RndbetPredictionsBackendApplication.java
@@ -0,0 +1,13 @@
+package rndbet.rndbetpredictionsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RndbetPredictionsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RndbetPredictionsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rndbet/rndbetpredictionsbackend/RndbetPredictionsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rndbet.rndbetpredictionsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RndbetPredictionsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
